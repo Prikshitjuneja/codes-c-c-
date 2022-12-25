dcbf2e0763dcb13386781701305c62a4624bc76e
Prikshitjuneja <121381078+Prikshitjuneja@users.noreply.github.com>
2022-12-25 16:43:03 +0530

    Update binarytreeuse.cpp

--- binarytreeuse.cpp
+++ binarytreeuse.cpp
@@ -106,7 +106,28 @@ int main()
     cout<<"height: "<<p.first<<endl;
     cout<<"diameter: "<<p.second<<endl;
     delete root;
-
-
-
 }
+  
+//binarytreeheaderfile
+template <typename T>
+class BinaryTreeNode
+{
+  public:
+  T data;
+  BinaryTreeNode<T> *left;
+  BinaryTreeNode<T>* right;
+
+  BinaryTreeNode(T data)
+  {
+      this->data=data;
+      left=NULL;
+      right=NULL;
+
+  }
+  ~BinaryTreeNode()
+  {
+      delete left;
+      delete right;
+  }
+
+};
